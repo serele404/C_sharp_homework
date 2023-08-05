ef9bb88dfe93ff7fd2a40d1a3678730ffa6e67ca
Elena Serova <>
2023-08-06 00:15:01 +0300

    add Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,19 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+int result = arg1;
+if(arg2>result) result=arg2;
+if(arg3>result) result=arg3;
+return result;
+}
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+//int max = Max(number1, number2, number3);
+
+Console.Write("Максимальное число из трех: ");
+Console.WriteLine(Max(number1, number2, number3));
