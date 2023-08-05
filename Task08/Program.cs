70f4d307cdae7d8e2a8709bcc416356d972bb664
Elena Serova <>
2023-08-06 01:29:42 +0300

    add Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int count = 0;
+int EvenNumberPlus = 2;
+int EvenNumberMinus = 0;
+
+if (number < 0)
+    {
+        while (EvenNumberMinus >= number)
+        {
+            {
+            Console.WriteLine(EvenNumberMinus);
+            EvenNumberMinus = EvenNumberMinus - 2;
+            }
+        count++;
+        }
+    }
+else 
+    if (number == 0)
+        {
+            Console.WriteLine("Введите число, отличное от нуля");
+        }
+else
+     if (number > 0)   
+        while (EvenNumberPlus <= number)
+        {
+            {
+            Console.WriteLine(EvenNumberPlus);
+            EvenNumberPlus = EvenNumberPlus + 2;
+            }
+        count++;
+        }
+
