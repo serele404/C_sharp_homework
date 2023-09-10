1fee6f3cd5684e8c9ea7bb58092b6ff3d7236891
Elena Serova <>
2023-09-10 22:03:30 +0300

    add Task54

---
+++ Task54/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит 
+// по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[,] Create2DArray(int rows, int columns)
+{
+    return new int[rows, columns];
+}
+
+void Fill2DArray(int[,] array, int minValue, int maxValue)
+{
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = random.Next(minValue, maxValue + 1);
+}
+
+void SortArray(int[,] array)
+{   
+    for (int i = 0; i < array.GetLength(0); i++)
+        {   
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                for (int k = j+1; k < array.GetLength(1); k++)
+                    {
+                    if (array[i,k] > array[i,j])
+                    {
+                    int tmp = array[i,j];
+                    array[i,j] = array[i,k];
+                    array[i,k] = tmp;
+                    }
+                }
+            }
+        }
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]}\t");
+        Console.WriteLine();
+    }
+}
+
+int rows = InputNum("Введите количество строк: ");
+int columns = InputNum("Введите количество столбцов: ");
+
+int[,] myArray = Create2DArray(rows, columns);
+
+int min = InputNum("Введите минимальное значение элемента: ");
+int max = InputNum("Введите максимальное значение элемента: ");
+
+Fill2DArray(myArray, min, max);
+Print2DArray(myArray);
+SortArray(myArray);
+Console.WriteLine();
+Print2DArray(myArray);
